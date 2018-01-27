30038b8db05d2fb7e4c94393c16a6534bd8c3efa
wbounas <wbounas@gmail.com>
2018-01-27 17:10:44 -0500

    Add relationships to ingredients and users
    -  Ingredients belong_to user
    -  User has_many ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,5 @@
 class IngredientsController < ProtectedController
-  before_action :set_ingredient, only: [:show, :update, :destroy]
+  before_action :set_ingredient, only: %i[show update destroy]
 
   # GET /ingredients
   def index
@@ -39,13 +39,14 @@ class IngredientsController < ProtectedController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_ingredient
-      @ingredient = Ingredient.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def ingredient_params
-      params.require(:ingredient).permit(:name, :unit, :quantity, :notes, :future)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :unit, :quantity, :notes, :future)
+  end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,4 @@
 class Ingredient < ApplicationRecord
+  belongs_to :user
+  validates :name, :unit, :quantity, :future, :user, presence: true
 end
