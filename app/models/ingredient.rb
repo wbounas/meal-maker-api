482f8bd39ea0477dc8729841a9a4122e3ccb995d
wbounas <wbounas@gmail.com>
2018-01-31 02:01:42 -0500

    Added on: :create validation option for Ingredient model
    -  Now Ingredient model only requires `:name`, `:unit`, `:quantity`,
    and `:user` on a Ingredient#create action, instead of all actions

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,5 +2,5 @@
 
 class Ingredient < ApplicationRecord
   belongs_to :user
-  validates :name, :unit, :quantity, :user, presence: true
+  validates :name, :unit, :quantity, :user, presence: true, on: :create
 end
